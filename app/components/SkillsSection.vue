8eab7e8a7caf0b152b3707942cb6acbc878dab8b
Samuel Poulin <samuel-poulin@outlook.com>
2026-02-16 02:32:47 -0500

    Refactor SkillsSection to use Tailwind for styling

--- app/components/SkillsSection.vue
+++ app/components/SkillsSection.vue
@@ -1,114 +1,40 @@
 <template>
-    <div id="skills-container" v-if="result?.Skills">
+    <div
+        v-if="result?.Skills"
+        class="flex flex-col py-[100px] gap-[10px] max-lg:py-[50px] max-md:py-[25px]"
+    >
         <a id="skills" />
         <div class="section-title-container">
             <h2 class="section-title"># things I picked up along the way</h2>
         </div>
-        <div id="skills-list">
+        <div class="flex flex-wrap gap-[10px]">
             <div
-                class="skill-container"
+                class="flex flex-col gap-[10px] p-[25px] rounded-[10px] bg-[#ececec] max-lg:p-[15px] max-md:items-center max-md:flex-grow"
                 v-for="skill of result.Skills.docs"
                 :key="skill.name"
             >
-                <div class="skill-informations">
+                <div class="flex items-center gap-[10px]">
                     <NuxtImg
-                        class="skill-logo"
+                        class="max-lg:w-[30px] max-lg:h-[30px]"
                         :src="skill.logo.url"
                         :alt="skill.logo.alt"
                         format="webp"
                     />
-                    <div class="skill-name">{{ skill.name }}</div>
+                    <div
+                        class="select-none font-medium text-[32px] max-lg:text-[24px]"
+                    >
+                        {{ skill.name }}
+                    </div>
                 </div>
-                <div class="skill-experience">
+                <div
+                    class="select-none font-normal text-[20px] max-lg:text-[16px]"
+                >
                     Experience: ~{{ skill.experience }}
                 </div>
             </div>
         </div>
     </div>
 </template>
-
-<style scoped>
-#skills-container {
-    display: flex;
-    flex-direction: column;
-
-    padding: 100px 0px;
-    gap: 10px;
-}
-
-#skills-list {
-    display: flex;
-    flex-wrap: wrap;
-
-    gap: 10px;
-}
-
-.skill-container {
-    display: flex;
-    flex-direction: column;
-
-    gap: 10px;
-    padding: 25px;
-
-    border-radius: 10px;
-    background-color: #ececec;
-}
-
-.skill-informations {
-    display: flex;
-    align-items: center;
-
-    gap: 10px;
-}
-
-.skill-name {
-    user-select: none;
-    font-weight: 500;
-    font-size: 32px;
-}
-
-.skill-experience {
-    user-select: none;
-    font-weight: 400;
-    font-size: 20px;
-}
-
-/* lg */
-@media only screen and (max-width: 1024px) {
-    #skills-container {
-        padding: 50px 0px;
-    }
-
-    .skill-container {
-        padding: 15px;
-    }
-
-    .skill-name {
-        font-size: 24px;
-    }
-
-    .skill-experience {
-        font-size: 16px;
-    }
-
-    .skill-logo {
-        width: 30px;
-        height: 30px;
-    }
-}
-
-/* md */
-@media only screen and (max-width: 768px) {
-    #skills-container {
-        padding: 25px 0px;
-    }
-
-    .skill-container {
-        align-items: center;
-        flex-grow: 1;
-    }
-}
-</style>
 
 <script setup lang="ts">
 const { result } = defineProps(["result"]);
