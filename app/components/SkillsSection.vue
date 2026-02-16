b7e2d35e4ca647d6be12c99b304f59b0e896944b
Samuel Poulin <samuel-poulin@outlook.com>
2026-02-16 04:26:13 -0500

    Simplify sorting

--- app/components/SkillsSection.vue
+++ app/components/SkillsSection.vue
@@ -50,18 +50,9 @@ const { data } = await useAsyncGql({
     operation: "skills",
 });
 
-const shuffleArray = <T,>(array: T[]): T[] => {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-};
-
 const shuffledSkills = computed(() => {
     if (!data?.value?.Skills?.docs) return [];
-    return shuffleArray(data?.value?.Skills.docs);
+    return [...data.value.Skills.docs].toSorted(() => Math.random() - 0.5);
 });
 
 const firstRowSkills = computed(() => {
