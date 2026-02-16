4f5341363d673298010683984141379ca38c8604
Samuel Poulin <samuel-poulin@outlook.com>
2026-02-15 22:51:36 -0500

    Improve header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="flex items-center justify-between w-full h-24">
-        <nuxt-link href="/" class="font-bold text-xl no-underline"
-            >SP</nuxt-link
-        >
+        <NuxtLink href="/" class="font-bold text-xl no-underline">SP</NuxtLink>
         <div class="hidden items-center gap-2.5 select-none lg:flex">
             <div class="flex items-center text-3xl">
                 {{ result?.HeaderAlert?.emojiLeft }}
             </div>
-            <div class="font-normal text-xl">
+            <div class="text-xl">
                 {{ result?.HeaderAlert?.alert }}
             </div>
             <div class="flex items-center text-3xl">
@@ -15,14 +13,14 @@
             </div>
         </div>
         <div class="flex gap-2.5 sm:gap-6 lg:gap-12 text-xl">
-            <NuxtLink to="#projects">projects</NuxtLink>
-            <NuxtLink to="#about">about</NuxtLink>
-            <NuxtLink to="#skills">skills</NuxtLink>
+            <NuxtLink class="underline" to="#projects">projects</NuxtLink>
+            <NuxtLink class="underline" to="#about">about</NuxtLink>
+            <NuxtLink class="underline" to="#skills">skills</NuxtLink>
         </div>
-        <nuxt-link
-            class="hidden text-xl sm:inline"
+        <NuxtLink
+            class="hidden text-xl sm:inline underline"
             href="mailto:samuel-poulin@outlook.com"
-            >contact me</nuxt-link
+            >contact me</NuxtLink
         >
     </div>
 </template>
