4a45352a95f518c23ece2201ef244ab7d521b89f
Samuel Poulin <samuel-poulin@outlook.com>
2026-02-15 22:20:56 -0500

    Improve header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,103 +1,31 @@
 <template>
-    <div id="header-container">
-        <nuxt-link id="logo-link" href="/">SP</nuxt-link>
-        <div id="header-alert-container">
-            <div class="header-alert-emoji">
+    <div class="flex items-center justify-between w-full h-24">
+        <nuxt-link href="/" class="font-bold text-xl no-underline"
+            >SP</nuxt-link
+        >
+        <div class="hidden items-center gap-2.5 select-none lg:flex">
+            <div class="flex items-center text-3xl">
                 {{ result?.HeaderAlert?.emojiLeft }}
             </div>
-            <div id="header-alert-text">
+            <div class="font-normal text-xl">
                 {{ result?.HeaderAlert?.alert }}
             </div>
-            <div class="header-alert-emoji">
+            <div class="flex items-center text-3xl">
                 {{ result?.HeaderAlert?.emojiRight }}
             </div>
         </div>
-        <div id="header-links-container">
-            <NuxtLink class="header-link" to="#projects">projects</NuxtLink>
-            <NuxtLink class="header-link" to="#about">about</NuxtLink>
-            <NuxtLink class="header-link" to="#skills">skills</NuxtLink>
+        <div class="flex gap-2.5 sm:gap-6 lg:gap-12 text-xl">
+            <NuxtLink to="#projects">projects</NuxtLink>
+            <NuxtLink to="#about">about</NuxtLink>
+            <NuxtLink to="#skills">skills</NuxtLink>
         </div>
         <nuxt-link
-            class="header-link"
-            id="header-contact-link"
+            class="hidden text-xl sm:inline"
             href="mailto:samuel-poulin@outlook.com"
             >contact me</nuxt-link
         >
     </div>
 </template>
-
-<style>
-#header-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    height: 100px;
-    width: 100%;
-}
-
-#logo-link {
-    color: #000000;
-    text-decoration: none;
-    font-weight: 700;
-    font-size: 20px;
-}
-
-#header-alert-container {
-    display: flex;
-    align-items: center;
-
-    gap: 10px;
-
-    user-select: none;
-}
-
-.header-alert-emoji {
-    display: flex;
-    align-items: center;
-
-    font-size: 32px;
-}
-
-#header-alert-text {
-    font-weight: 400;
-    font-size: 20px;
-}
-
-#header-links-container {
-    display: flex;
-
-    gap: 50px;
-}
-
-.header-link {
-    color: #000000;
-    font-weight: 400;
-    font-size: 20px;
-}
-
-/* lg */
-@media only screen and (max-width: 1024px) {
-    #header-links-container {
-        gap: 25px;
-    }
-
-    #header-alert-container {
-        display: none;
-    }
-}
-
-/* sm */
-@media only screen and (max-width: 640px) {
-    #header-links-container {
-        gap: 10px;
-    }
-
-    #header-contact-link {
-        display: none;
-    }
-}
-</style>
 
 <script lang="ts" setup>
 const { data: result } = await useAsyncGql({
